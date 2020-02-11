3e5f3041bcc3a26e43c670cc59c82026ad8637f9
Francis Lavoie <lav.francis+github@gmail.com>
2020-02-10 23:10:22 -0500

    updated dependencies and babel config

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { storiesOf, action, linkTo } from '@storybook/react';
-import Welcome from './Welcome';
 
 import cardStory from '../src/containers/card.story';
 import scrollViewStory from '../src/containers/scrollview.story';
@@ -8,8 +7,6 @@ import layoutStory from '../src/layouts/layout.story';
 import buttonStory from '../src/widgets/button.story';
 import inputStory from '../src/widgets/input.story';
 import labelStory from '../src/widgets/label.story';
-
-import { containerSizeStyles } from './config';
 
 storiesOf('Containers', module)
     .add('Card', cardStory)
